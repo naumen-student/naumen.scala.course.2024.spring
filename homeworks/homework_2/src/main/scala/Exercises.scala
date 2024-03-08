57e56d050eccc1c97427a26682c76c90bfae9b0e
Arseniy <taraskin.arseniy@gmail.com>
2024-03-08 19:50:14 +0500

    homework2 - Тараскин Арсений

--- homeworks/homework_2/src/main/scala/Exercises.scala
+++ homeworks/homework_2/src/main/scala/Exercises.scala
@@ -16,7 +16,10 @@ object Exercises {
     /*Реализовать функцию, которая возвращает сумму всех целых чисел в заданном диапазоне (от iForm до iTo), которые делятся
     на 3 или на 5.*/
     /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
-    def sumOfDivBy3Or5(iFrom: Int, iTo: Int): Long = ???
+    def sumOfDivBy3Or5(iFrom: Int, iTo: Int): Long = {
+        (iFrom to iTo).filter(x => x % 3 == 0 || x % 5 == 0).sum
+
+    }
 
 
 
@@ -25,7 +28,23 @@ object Exercises {
     Число 80 раскладывается на множители 1 * 2 * 2 * 2 * 2 * 5, результат выполнения функции => Seq(2, 5).
     Число 98 можно разложить на множители 1 * 2 * 7 * 7, результат выполнения функции => Seq(2, 7).*/
     /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
-    def primeFactor(number: Int): Seq[Int] = ???
+    def primeFactor(number: Int): Seq[Int] = {
+        require(number > 0, "Число должно быть положительным")
+
+        def isPrime(n: Int): Boolean = {
+            if (n <= 1) false
+            else if (n == 2) true
+            else !(2 to Math.sqrt(n).toInt).exists(x => n % x == 0)
+        }
+
+        def primeFactorsHelper(n: Int, divisor: Int = 2, acc: Seq[Int] = Seq()): Seq[Int] = {
+            if (n <= 1) acc
+            else if (n % divisor == 0 && isPrime(divisor)) primeFactorsHelper(n / divisor, divisor + 1, acc :+ divisor)
+            else primeFactorsHelper(n, divisor + 1, acc)
+        }
+
+        primeFactorsHelper(number)
+    }
 
 
 
@@ -40,15 +59,16 @@ object Exercises {
     def abs(vec: Vector2D): Double = java.lang.Math.sqrt(vec.x * vec.x + vec.y * vec.y)
     def scalar(vec0: Vector2D, vec1: Vector2D): Double = vec0.x * vec1.x + vec0.y * vec1.y
     def cosBetween(vec0: Vector2D, vec1: Vector2D): Double = scalar(vec0, vec1) / abs(vec0) / abs(vec1)
-    //def sumByFunc(leftVec0: Vector2D, leftVec1: Vector2D, ???, rightVec0: Vector2D, rightVec1: Vector2D) = ???
-    /*
+
+
+    def sumByFunc(leftVec0: Vector2D, leftVec1: Vector2D, ???, rightVec0: Vector2D, rightVec1: Vector2D) = func(leftVec0, leftVec1) +
+      func(rightVec0, rightVec1)
     def sumScalars(leftVec0: Vector2D, leftVec1: Vector2D, rightVec0: Vector2D, rightVec1: Vector2D): Double =
         sumByFunc(leftVec0, leftVec1, scalar, rightVec0, rightVec1)
-    */
-    /*
+
     def sumCosines(leftVec0: Vector2D, leftVec1: Vector2D, rightVec0: Vector2D, rightVec1: Vector2D): Double =
         sumByFunc(leftVec0, leftVec1, cosBetween, rightVec0, rightVec1)
-    */
+
 
 
 
@@ -71,6 +91,10 @@ object Exercises {
             "Chrome" ->   (3,   7.18),   "Cesium" ->    (7,   1.873), "Zirconium" -> (3,   6.45)
         )
 
-    def sortByHeavyweight(ballsArray: Map[String, (Int, Double)] = balls): Seq[String] = ???
+    def sortByHeavyweight(ballsArray: Map[String, (Int, Double)] = balls): Seq[String] = {
+        ballsArray.keys.toSeq.sortBy(material => { val (radius, density) = ballsArray(material)
+            density * Math.pow(radius, 3) * Math.PI
+        })
+    }
 
 }
--- homeworks/homework_2/src/test/scala/Test.scala
+++ homeworks/homework_2/src/test/scala/Test.scala
@@ -8,5 +8,52 @@ object Test extends TestSuite{
             assert(Exercises.divBy3Or7(5, 9) == Seq(6, 7, 9))
             assert(Exercises.divBy3Or7(0, 100) == Seq(0, 3, 6, 7, 9, 12, 14, 15, 18, 21, 24, 27, 28, 30, 33, 35, 36, 39, 42, 45, 48, 49, 51, 54, 56, 57, 60, 63, 66, 69, 70, 72, 75, 77, 78, 81, 84, 87, 90, 91, 93, 96, 98, 99))
         }
+        'test_sumOfDivBy3Or5 - {
+            assert(Exercises.sumOfDivBy3Or5(1, 10) == 23)
+            assert(Exercises.sumOfDivBy3Or5(1, 20) == 78)
+            assert(Exercises.sumOfDivBy3Or5(-10, 10) == 0)
+        }
+        'test_primeFactor - {
+            assert(Exercises.primeFactor(32) == Seq(2))
+            assert(Exercises.primeFactor(87) == Seq(3, 29))
+            assert(Exercises.primeFactor(74) == Seq(2, 37))
+            assert(Exercises.primeFactor(80) == Seq(2, 5))
+            assert(Exercises.primeFactor(98) == Seq(2, 7))
+            assert(Exercises.primeFactor(1) == Seq())
+            assert(Exercises.primeFactor(17) == Seq(17))
+            assert(Exercises.primeFactor(0) == Seq())
+            assert(Exercises.primeFactor(-20) == Seq())
+        }
+
+        'test_sumByFunc'{
+            assert(Math.abs(Exercises.sumScalars(Exercises.Vector2D(3, 4), Exercises.Vector2D(1, 5), Exercises.Vector2D(7, 2), Exercises.Vector2D(4, 8))) == 70)
+
+            assert(Math.abs(Exercises.sumCosines(Exercises.Vector2D(3, 4), Exercises.Vector2D(1, 5), Exercises.Vector2D(7, 2), Exercises.Vector2D(4, 8))) == 2.205450507455369)
+
+            assert(Math.abs(Exercises.sumScalars(Exercises.Vector2D(7, 2), Exercises.Vector2D(3, 4), Exercises.Vector2D(1, 5), Exercises.Vector2D(4, 8))) == 3.2054505074553691)
+
+        }
+        'test_sortByHeavyweight - {
+            val balls: Map[String, (Int, Double)] =
+                Map(
+                    "Aluminum" -> (3, 2.6889), "Tungsten" -> (2, 19.35), "Graphite" -> (12, 2.1), "Iron" -> (3, 7.874),
+                    "Gold" -> (2, 19.32), "Potassium" -> (14, 0.862), "Calcium" -> (8, 1.55), "Cobalt" -> (4, 8.90),
+                    "Lithium" -> (12, 0.534), "Magnesium" -> (10, 1.738), "Copper" -> (3, 8.96), "Sodium" -> (5, 0.971),
+                    "Nickel" -> (2, 8.91), "Tin" -> (1, 7.29), "Platinum" -> (1, 21.45), "Plutonium" -> (3, 19.25),
+                    "Lead" -> (2, 11.336), "Titanium" -> (2, 10.50), "Silver" -> (4, 4.505), "Uranium" -> (2, 19.04),
+                    "Chrome" -> (3, 7.18), "Cesium" -> (7, 1.873), "Zirconium" -> (3, 6.45)
+                )
+
+            val exp = List(
+                "Lithium", "Sodium", "Potassium", "Graphite", "Cesium", "Calcium", "Nickel", "Chrome",
+
+                "Cobalt", "Zirconium", "Iron", "Lead", "Magnesium", "Aluminum", "Tin", "Tungsten",
+
+                "Titanium", "Uranium", "Gold", "Silver", "Platinum", "Plutonium", "Copper"
+            )
+
+            assert(Exercises.sortByHeavyweight(balls) == exp)
+        }
     }
+
 }
