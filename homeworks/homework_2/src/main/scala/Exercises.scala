2273180b413a18e59c44d13dd0df484e25c0d6ea
Andrey <dikovandrey856@gmail.com>
2024-03-13 01:39:18 +0500

    create homework 2

--- homeworks/homework_2/src/main/scala/Exercises.scala
+++ homeworks/homework_2/src/main/scala/Exercises.scala
@@ -4,11 +4,10 @@ object Exercises {
     /*Реализовать функцию, которая возвращает все целые числа в заданном диапазоне (от iForm до iTo), которые делятся
     на 3 или на 7.*/
     /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
-    def divBy3Or7(iFrom: Int, iTo: Int): Seq[Int] = {
-        for {i <- iFrom to iTo
-             if i % 3 == 0 || i % 7 == 0
-        } yield i
-    }
+    def divBy3Or7(iFrom: Int, iTo: Int): Seq[Int] = for {
+        i <- iFrom to iTo
+        if i % 3 == 0 || i % 7 == 0
+    } yield i
 
 
 
@@ -16,7 +15,9 @@ object Exercises {
     /*Реализовать функцию, которая возвращает сумму всех целых чисел в заданном диапазоне (от iForm до iTo), которые делятся
     на 3 или на 5.*/
     /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
-    def sumOfDivBy3Or5(iFrom: Int, iTo: Int): Long = ???
+    def sumOfDivBy3Or5(iFrom: Int, iTo: Int): Long = (iFrom to iTo)
+      .filter(i => i % 3 == 0 || i % 5 == 0)
+      .sum
 
 
 
@@ -25,8 +26,21 @@ object Exercises {
     Число 80 раскладывается на множители 1 * 2 * 2 * 2 * 2 * 5, результат выполнения функции => Seq(2, 5).
     Число 98 можно разложить на множители 1 * 2 * 7 * 7, результат выполнения функции => Seq(2, 7).*/
     /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
-    def primeFactor(number: Int): Seq[Int] = ???
-
+    def primeFactor(number: Int): Seq[Int] = {
+        if (number > 1 && !(2 until number).exists(x => number % x == 0)) Seq(number)
+        else {
+            var n = number
+            var result = Seq[Int]()
+            val limit = Math.sqrt(number).toInt
+            for (i <- 2 to limit) {
+                while (n % i == 0) {
+                    result = result :+ i
+                    n /= i
+                }
+            }
+            result.distinct
+        }
+    }
 
 
     /*ЗАДАНИЕ III*/
@@ -40,16 +54,18 @@ object Exercises {
     def abs(vec: Vector2D): Double = java.lang.Math.sqrt(vec.x * vec.x + vec.y * vec.y)
     def scalar(vec0: Vector2D, vec1: Vector2D): Double = vec0.x * vec1.x + vec0.y * vec1.y
     def cosBetween(vec0: Vector2D, vec1: Vector2D): Double = scalar(vec0, vec1) / abs(vec0) / abs(vec1)
-    //def sumByFunc(leftVec0: Vector2D, leftVec1: Vector2D, ???, rightVec0: Vector2D, rightVec1: Vector2D) = ???
-    /*
+    def sumByFunc(leftVec0: Vector2D, leftVec1: Vector2D,
+                  calculation: (Vector2D, Vector2D) => Double,
+                  rightVec0: Vector2D, rightVec1: Vector2D): Double =
+        calculation(leftVec0, leftVec1) + calculation(rightVec0, rightVec1)
+
     def sumScalars(leftVec0: Vector2D, leftVec1: Vector2D, rightVec0: Vector2D, rightVec1: Vector2D): Double =
         sumByFunc(leftVec0, leftVec1, scalar, rightVec0, rightVec1)
-    */
-    /*
-    def sumCosines(leftVec0: Vector2D, leftVec1: Vector2D, rightVec0: Vector2D, rightVec1: Vector2D): Double =
-        sumByFunc(leftVec0, leftVec1, cosBetween, rightVec0, rightVec1)
-    */
 
+
+    def sumCosines(leftVec0: Vector2D, leftVec1: Vector2D, rightVec0: Vector2D, rightVec1: Vector2D): Double = {
+        sumByFunc(leftVec0, leftVec1, cosBetween, rightVec0, rightVec1)
+    }
 
 
     /*ЗАДАНИЕ IV*/
@@ -61,8 +77,8 @@ object Exercises {
     В качестве значения числа "Пи" можно использовать java.lang.Math.PI
     */
     /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
-    val balls: Map[String, (Int, Double)] =
-        Map(
+    private val balls: Map[String, (Int, Double)] =
+        Map (
             "Aluminum" -> (3,   2.6889), "Tungsten" ->  (2,   19.35), "Graphite" ->  (12,  2.1),   "Iron" ->      (3,   7.874),
             "Gold" ->     (2,   19.32),  "Potassium" -> (14,  0.862), "Calcium" ->   (8,   1.55),  "Cobalt" ->    (4,   8.90),
             "Lithium" ->  (12,  0.534),  "Magnesium" -> (10,  1.738), "Copper" ->    (3,   8.96),  "Sodium" ->    (5,   0.971),
@@ -71,6 +87,9 @@ object Exercises {
             "Chrome" ->   (3,   7.18),   "Cesium" ->    (7,   1.873), "Zirconium" -> (3,   6.45)
         )
 
-    def sortByHeavyweight(ballsArray: Map[String, (Int, Double)] = balls): Seq[String] = ???
-
+    def sortByHeavyweight(ballsArray: Map[String, (Int, Double)] = balls): Seq[String] =
+        ballsArray.mapValues {case (radius, density) => (4 * Math.PI * Math.pow(radius, 3) * density) / 3}
+          .toSeq
+          .sortBy(_._2)
+          .map(_._1)
 }
