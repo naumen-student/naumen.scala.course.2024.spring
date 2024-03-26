3982dcdbd531dd0e4a73c993c0218f1d77146038
George <george.lapp.java@mail.ru>
2024-03-26 21:20:42 +0500

    Homework2

--- src/main/scala/sdfgh.scala
+++ src/main/scala/sdfgh.scala
@@ -1,37 +1,32 @@
-object Exercises {
+object sdfgh {
+  object Exercises {
 
     /*ПРИМЕР*/
     /*Реализовать функцию, которая возвращает все целые числа в заданном диапазоне (от iForm до iTo), которые делятся
     на 3 или на 7.*/
     /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
     def divBy3Or7(iFrom: Int, iTo: Int): Seq[Int] = {
-        for {i <- iFrom to iTo
-             if i % 3 == 0 || i % 7 == 0
-             } yield i
+      for {i <- iFrom to iTo
+           if i % 3 == 0 || i % 7 == 0
+           } yield i
     }
-
-
+    def main(args: Array[String]): Unit = {
+      println("Hello world!")
+    }
 
     /*ЗАДАНИЕ I*/
     /*Реализовать функцию, которая возвращает сумму всех целых чисел в заданном диапазоне (от iForm до iTo), которые делятся
     на 3 или на 5.*/
     /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
-    def sumOfDivBy3Or5(iFrom: Int, iTo: Int): Long = {
-        var s = 0l;
-        for {i <- iFrom to iTo
-             if i % 3 == 0 || i % 5 == 0
-             } s += i
-        return s
+    def sumOfDivBy3Or5(iFrom: Int, iTo: Int): Long =
+    {
+      var sum:Long=0
+      for {i <- iFrom to iTo
+           if i % 3 == 0 || i % 5 == 0
+           } sum+=i
+      return 1;
     }
 
-
-    def isSimple(c: Int): Boolean = {
-        var limit = Math.floor(Math.sqrt(c)).toInt
-        for (i <- 2 to limit
-             if c % i == 0)
-            return false
-        return true
-    }
 
 
     /*ЗАДАНИЕ II*/
@@ -39,13 +34,7 @@ object Exercises {
     Число 80 раскладывается на множители 1 * 2 * 2 * 2 * 2 * 5, результат выполнения функции => Seq(2, 5).
     Число 98 можно разложить на множители 1 * 2 * 7 * 7, результат выполнения функции => Seq(2, 7).*/
     /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
-    def primeFactor(number: Int): Seq[Int] = {
-        var limit = Math.floor(Math.sqrt(number)).toInt
-        for {i <- 2 to limit
-             if number % i == 0 && isSimple(i)
-             } yield i
-
-    }
+    def primeFactor(number: Int): Seq[Int] = ???
 
 
 
@@ -60,15 +49,15 @@ object Exercises {
     def abs(vec: Vector2D): Double = java.lang.Math.sqrt(vec.x * vec.x + vec.y * vec.y)
     def scalar(vec0: Vector2D, vec1: Vector2D): Double = vec0.x * vec1.x + vec0.y * vec1.y
     def cosBetween(vec0: Vector2D, vec1: Vector2D): Double = scalar(vec0, vec1) / abs(vec0) / abs(vec1)
-    def sumByFunc(leftVec0: Vector2D, leftVec1: Vector2D, func: (Vector2D, Vector2D) => Double, rightVec0: Vector2D, rightVec1: Vector2D) =
-        func(leftVec0, leftVec1) + func(rightVec0, rightVec1)
-
+    //def sumByFunc(leftVec0: Vector2D, leftVec1: Vector2D, ???, rightVec0: Vector2D, rightVec1: Vector2D) = ???
+    /*
     def sumScalars(leftVec0: Vector2D, leftVec1: Vector2D, rightVec0: Vector2D, rightVec1: Vector2D): Double =
         sumByFunc(leftVec0, leftVec1, scalar, rightVec0, rightVec1)
-
+    */
+    /*
     def sumCosines(leftVec0: Vector2D, leftVec1: Vector2D, rightVec0: Vector2D, rightVec1: Vector2D): Double =
         sumByFunc(leftVec0, leftVec1, cosBetween, rightVec0, rightVec1)
-
+    */
 
 
 
@@ -82,15 +71,16 @@ object Exercises {
     */
     /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
     val balls: Map[String, (Int, Double)] =
-        Map(
-            "Aluminum" -> (3,   2.6889), "Tungsten" ->  (2,   19.35), "Graphite" ->  (12,  2.1),   "Iron" ->      (3,   7.874),
-            "Gold" ->     (2,   19.32),  "Potassium" -> (14,  0.862), "Calcium" ->   (8,   1.55),  "Cobalt" ->    (4,   8.90),
-            "Lithium" ->  (12,  0.534),  "Magnesium" -> (10,  1.738), "Copper" ->    (3,   8.96),  "Sodium" ->    (5,   0.971),
-            "Nickel" ->   (2,   8.91),   "Tin" ->       (1,   7.29),  "Platinum" ->  (1,   21.45), "Plutonium" -> (3,   19.25),
-            "Lead" ->     (2,   11.336), "Titanium" ->  (2,   10.50), "Silver" ->    (4,   4.505), "Uranium" ->   (2,   19.04),
-            "Chrome" ->   (3,   7.18),   "Cesium" ->    (7,   1.873), "Zirconium" -> (3,   6.45)
-        )
+      Map(
+        "Aluminum" -> (3,   2.6889), "Tungsten" ->  (2,   19.35), "Graphite" ->  (12,  2.1),   "Iron" ->      (3,   7.874),
+        "Gold" ->     (2,   19.32),  "Potassium" -> (14,  0.862), "Calcium" ->   (8,   1.55),  "Cobalt" ->    (4,   8.90),
+        "Lithium" ->  (12,  0.534),  "Magnesium" -> (10,  1.738), "Copper" ->    (3,   8.96),  "Sodium" ->    (5,   0.971),
+        "Nickel" ->   (2,   8.91),   "Tin" ->       (1,   7.29),  "Platinum" ->  (1,   21.45), "Plutonium" -> (3,   19.25),
+        "Lead" ->     (2,   11.336), "Titanium" ->  (2,   10.50), "Silver" ->    (4,   4.505), "Uranium" ->   (2,   19.04),
+        "Chrome" ->   (3,   7.18),   "Cesium" ->    (7,   1.873), "Zirconium" -> (3,   6.45)
+      )
 
-    def sortByHeavyweight(ballsArray: Map[String, (Int, Double)] = balls): Seq[String] =
-        ballsArray.map {case (m, (r, d)) => (m, math.pow(r, 3) * d)}.toSeq.sortBy(_._2).map(_._1)
+    def sortByHeavyweight(ballsArray: Map[String, (Int, Double)] = balls): Seq[String] = ???
+
+  }
 }
