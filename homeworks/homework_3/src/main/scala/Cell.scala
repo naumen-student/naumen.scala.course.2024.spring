4d30cdb0226419db94c827495afb595670f62b07
SfBalaba <sf.balaba0170@gmail.com>
2024-03-28 23:12:26 +0500

    pass all tests

--- homeworks/homework_3/src/main/scala/Cell.scala
+++ homeworks/homework_3/src/main/scala/Cell.scala
@@ -13,9 +13,11 @@ class StringCell(str: String) extends Cell {
 }
 
 class ReferenceCell(ix: Int, iy: Int, table: Table) extends Cell {
+
+  private def getRef: Option[Cell] = table.getCell(ix, iy)
   override def toString: String = {
     table.getCell(ix, iy).map {
-      case referenceCell: ReferenceCell => referenceCell.toString
+      case referenceCell: ReferenceCell => if (referenceCell.getRef.contains(this)) "cyclic" else referenceCell.toString
       case cell: Cell => cell.toString
     }getOrElse("outOfRange")
   }
