3eccddace35d6bc5ea8bed468e77d350d73b9197
Vladimir_Perepelkin <42518856+PigR@users.noreply.github.com>
2024-05-28 16:14:31 +0500

    hw5

--- homeworks/homework_5/src/main/scala/Task1.scala
+++ homeworks/homework_5/src/main/scala/Task1.scala
@@ -1,32 +1,7 @@
-/*
-  Задание №1
-  В задание уже описан тайп класс и синтакс для него.
-  Вам необходимо в объекте ShowInstance описать инстансы тайп класса
-  для типа Cat и Box.
-  Тип Cat, в соответствии с тем, какого конкретно наследника этого типа мы хотим показать,
-  должен отображаться следующим образом:
-  VeryLittleCat - очень маленький кот его_имя
-  LittleCat - маленький кот его_имя
-  NormalCat - кот его_имя
-  BigCat - большой кот его_имя
-  VeryBigCat - очень большой кот его_имя
-
-  Если кот будет в коробке, то к тому, что должно выводиться для кота
-  необходимо добавить "в коробке". Если коробка пустая, то выводить "пустая коробка"
-
-  В тестах можно всегда более точно посмотреть фразы.
- */
 object Task1 extends App {
-  // Обратите внимание, что здесь type class параматрезирован контрвариантно
-  // благодаря этому инстанс Show[Cat] мы можем применить, например, к BigCat.
-  // Однако, это может быть неудобно, так как нам для каждого нового наследника придётся менять
-  // реализацию Show[Cat]. Из-за этого практически все библиотеки, которые предоставляют свои
-  // тайп классы для их использования пользователем, делают тайп классы инвариантными по параметру.
-  // Можете написать две реализации (при -А и А) и сравнить их.
   trait Show[-A] {
     def show(a: A): String
   }
-
   sealed trait Cat {
     def name: String
   }
@@ -35,7 +10,6 @@ object Task1 extends App {
   case class NormalCat(name: String) extends Cat
   case class BigCat(name: String) extends Cat
   case class VeryBigCat(name: String) extends Cat
-
   sealed trait Box[+A] {
     def value: A
   }
@@ -46,8 +20,21 @@ object Task1 extends App {
 
   object ShowInstance {
     implicit val catShow: Show[Cat] = ???
+    import Task1.ShowSyntax.ShowOps
 
     implicit def boxShow[A: Show]: Show[Box[A]] = ???
+    implicit val catShow: Show[Cat] = {
+      case VeryLittleCat(name) => s"очень маленький кот $name"
+      case LittleCat(name) => s"маленький кот $name"
+      case NormalCat(name) => s"кот $name"
+      case BigCat(name) => s"большой кот $name"
+      case VeryBigCat(name) => s"очень большой кот $name"
+    }
+
+    implicit def boxShow[A: Show]: Show[Box[A]] = {
+      case EmptyBox => "пустая коробка"
+      case BoxWith(cat) => s"${cat.show} в коробке"
+    }
   }
 
   object ShowSyntax {
--- homeworks/homework_5/src/main/scala/Task2.scala
+++ homeworks/homework_5/src/main/scala/Task2.scala
@@ -1,34 +1,46 @@
 import cats._
 import cats.implicits._
 
-/*
-  Задание №2
-  Всё просто, для каждого кейс класса необходимо описать логику его сложения.
-  Радиус-вектор должен складываться, как и любой другой вектор.
-  GradeAngle всегда выражает угол [0, 360).
-  SquareMatrix просто сложение квадратных матриц
- */
 object Task2 extends App {
   case class RadiusVector(x: Int, y: Int)
   object RadiusVector {
     implicit val monoid: Monoid[RadiusVector] = ???
+    implicit val monoid: Monoid[RadiusVector] = new Monoid[RadiusVector] {
+      override def empty: RadiusVector = RadiusVector(0, 0)
+
+      override def combine(vec1: RadiusVector, vec2: RadiusVector): RadiusVector =
+        RadiusVector(vec1.x + vec2.x, vec1.y + vec2.y)
+    }
   }
   case class DegreeAngle(angel: Double)
   object DegreeAngle {
     implicit val monoid: Monoid[DegreeAngle] = ???
+    implicit val monoid: Monoid[DegreeAngle] = new Monoid[DegreeAngle] {
+      override def empty: DegreeAngle = DegreeAngle(0)
+
+      override def combine(a1: DegreeAngle, a2: DegreeAngle): DegreeAngle = DegreeAngle((a1.angel + a2.angel) % 360)
+    }
   }
 
   case class SquareMatrix[A : Monoid](values: ((A, A, A), (A, A, A), (A, A, A)))
   object SquareMatrix {
     implicit def monoid[A: Monoid]: Monoid[SquareMatrix[A]] = ???
+    implicit def monoid[A: Monoid]: Monoid[SquareMatrix[A]] = new Monoid[SquareMatrix[A]] {
+      override def empty: SquareMatrix[A] = {
+        val row = (Monoid[A].empty, Monoid[A].empty, Monoid[A].empty)
+        SquareMatrix(row.copy(), row.copy(), row.copy())
+      }
+
+      override def combine(m1: SquareMatrix[A], m2: SquareMatrix[A]): SquareMatrix[A] = {
+        new SquareMatrix[A](m1.values.combine(m2.values))
+      }
+    }
   }
 
   val radiusVectors = Vector(RadiusVector(0, 0), RadiusVector(0, 1), RadiusVector(-1, 1))
   Monoid[RadiusVector].combineAll(radiusVectors) // RadiusVector(-1, 2)
-
   val gradeAngles = Vector(DegreeAngle(380), DegreeAngle(60), DegreeAngle(30))
   Monoid[DegreeAngle].combineAll(gradeAngles) // GradeAngle(90)
-
   val matrixes = Vector(
     SquareMatrix(
       (
