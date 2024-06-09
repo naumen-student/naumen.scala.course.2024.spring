43de62be86c94a8423828f97667e478289b126d4
khostya <khostya.konstantin@gmail.com>
2024-06-09 23:29:14 +0500

    hw_4

--- homeworks/homework_4/src/main/scala/Exercises.scala
+++ homeworks/homework_4/src/main/scala/Exercises.scala
@@ -1,19 +1,12 @@
+import scala.annotation.tailrec
+import scala.util.{Random, Try, Success, Failure}
 
 object Exercises {
 
-    /**
-     * Задание №1
-     * Дана императивная функция findSumImperative.
-     * Напишите ее аналог (findSumFunctional) в функциональном стиле.
-     *
-     * ПОДСКАЗКА
-     * Стоит воспользоваться методами, которые предоставляет объект List или рекурсией.
-     * Страница с полезностями List: https://alvinalexander.com/scala/list-class-methods-examples-syntax/
-     */
     def findSumImperative(items: List[Int], sumValue: Int): (Int, Int) = {
         var result: (Int, Int) = (-1, -1)
         for (i <- 0 until items.length) {
-            for (j <- 0 until items.length) {
+            for (j <- items.indices) {
                 if (items(i) + items(j) == sumValue && i != j) {
                     result = (i, j)
                 }
@@ -23,19 +16,15 @@ object Exercises {
     }
 
     def findSumFunctional(items: List[Int], sumValue: Int) = {
-        (-1, -1)
+      items.indices.flatMap { x =>
+        items.indices.filter { y =>
+          items(x) + items(y) == sumValue && x != y
+        }.map(j => (x, j))
+      }.lastOption.getOrElse((-1, -1));
     }
 
 
-    /**
-     * Задание №2
-     *
-     * Дана рекурсивная функция simpleRecursion.
-     * Перепишите ее так, чтобы получилась хвостовая рекурсивная функция.
-     *
-     * Для прохождения теста на большое количество элементов в списке
-     * используйте анотацию @tailrec к вашей функции.
-     */
+
     def simpleRecursion(items: List[Int], index: Int = 1): Int = {
         items match {
             case head :: tail =>
@@ -49,7 +38,20 @@ object Exercises {
     }
 
     def tailRecRecursion(items: List[Int]): Int = {
-        1
+      @tailrec
+      def rec(items: List[Int], index: Int = 1, sum: Int): Int = {
+        items match {
+          case head :: tail =>
+            if (head % 2 == 0) {
+              rec(tail, index - 1, index + (head * sum))
+            } else {
+              rec(tail, index - 1, index - (head * sum))
+            }
+          case _ => sum
+        }
+      }
+
+      rec(items.reverse, items.length, 1)
     }
 
     /**
@@ -60,40 +62,37 @@ object Exercises {
      */
 
     def functionalBinarySearch(items: List[Int], value: Int): Option[Int] = {
-        None
+      @tailrec
+      def binarySearch(l: Int, r: Int): Option[Int] = {
+        Some((l + r) / 2).filter(_ => l <= r) match {
+          case None => None
+          case Some(m) =>
+            if (items(m) == value)
+              Some(m)
+            else if (items(m) < value)
+              binarySearch(m + 1, r)
+            else
+              binarySearch(l, m - 1)
+        }
+      }
+
+      binarySearch(0, items.length - 1)
     }
 
-    /**
-     * Задание №4
-     * Реализуйте функцию, которая генерирует список заданной длинны c именами.
-     * Функция должна соответствовать всем правилам функционального программирования.
-     *
-     * Именем является строка, не содержащая иных символов, кроме буквенных, а также начинающаяся с заглавной буквы.
-     */
 
-    def generateNames(namesСount: Int): List[String] = {
-        if (namesСount < 0) throw new Throwable("Invalid namesCount")
-        Nil
+    def generateNames(count: Int): List[String] = {
+        if (count < 0)
+          throw new Throwable("Invalid namesCount")
+        else 
+          List.tabulate(count) { _ =>
+            Random.alphanumeric.filter(_.isLetter).
+              take(4 + Random.nextInt(15)).
+              mkString.
+              toLowerCase.capitalize
+          }
     }
 
 }
-
-/**
- * Задание №5
- *
- * Дана реализация сервиса по смене номера SimpleChangePhoneService с методом changePhone
- * Необходимо написать реализацию этого сервиса с учетом правил работы со сторонними эффектами (SideEffects).
- *
- * Для этого необходимо сначала реализовать собственный сервис работы с телефонными номерами (PhoneServiceSafety),
- * используя при этом методы из unsafePhoneService.
- * Методы должны быть безопасными, поэтому тип возвращаемых значений необходимо определить самостоятельно.
- * Рекомендуется воспользоваться стандартными типами Scala (например Option или Either).
- *
- * Затем, с использованием нового сервиса, необходимо реализовать "безопасную" версию функции changePhone.
- * Функция должна возвращать ok в случае успешного завершения или текст ошибки.
- *
- * Изменять методы внутри SimplePhoneService не разрешается.
- */
 
 object SideEffectExercise {
     import Utils._
@@ -111,14 +110,29 @@ object SideEffectExercise {
 
 
     class PhoneServiceSafety(unsafePhoneService: SimplePhoneService) {
-        def findPhoneNumberSafe(num: String) = ???
+        def findPhoneNumberSafe(num: String): Option[String] = {
+          Option(unsafePhoneService.findPhoneNumber(num))
+        }
 
-        def addPhoneToBaseSafe(phone: String) = ???
+        def addPhoneToBaseSafe(phone: String): Either[String, Unit] = {
+          Try(unsafePhoneService.addPhoneToBase(phone)) match {
+            case Success(ok) => Right(ok)
+            case Failure(exception) => Left(exception.getMessage)
+          }
+        }
 
-        def deletePhone(phone: String) = ???
+        def deletePhone(phone: String): Option[Unit] = {
+          Option(findPhoneNumberSafe(phone).map(unsafePhoneService.deletePhone))
+        }
     }
 
     class ChangePhoneServiceSafe(phoneServiceSafety: PhoneServiceSafety) extends ChangePhoneService {
-        override def changePhone(oldPhone: String, newPhone: String): String = ???
+      override def changePhone(oldPhone: String, newPhone: String): String = {
+        phoneServiceSafety.findPhoneNumberSafe(oldPhone).foreach(phoneServiceSafety.deletePhone)
+        phoneServiceSafety.addPhoneToBaseSafe(newPhone) match {
+          case Right(_) => "ok"
+          case Left(message) => message
+        }
+      }
     }
 }
