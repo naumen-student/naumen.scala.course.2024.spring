e32d6df09b1c06a6162b140e9eb3574686c13003
Mikstyrka <112823533+Mikstyrka@users.noreply.github.com>
2024-06-10 21:17:50 +0500

    Add files via upload

--- homeworks/homework_5/src/main/scala/Task3.scala
+++ homeworks/homework_5/src/main/scala/Task3.scala
@@ -1,32 +1,38 @@
-import cats._
-import cats.implicits._
-
-import scala.concurrent.{Await, Future}
-import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.DurationInt
+import scala.concurrent.{Await, Future}
 
-/*
-  Задание №3
-  Всё просто, нужно посчитать количество строк.
-  Реализуйте функцию countWords, которая принимает список строк.
-  Обязательно использовать функцию mapReduce.
- */
+@@ -11,6 +10,7 @@
+
+
+
 object Task3 extends App {
   def mapReduce[A, B: Monoid](values: Vector[A])(func: A => B): Future[B] = {
     val numCores = Runtime.getRuntime.availableProcessors
-    val groupSize = (1.0 * values.size / numCores).ceil.toInt
-    values
-      .grouped(groupSize)
-      .toVector
-      .traverse(group => Future(group.foldMap(func)))
-      .map(_.combineAll)
-  }
+    @@ - 25, 8 + 25, 20 @@
+    object Task3 extends App {
+      case class Count(word: String, count: Int)
 
-  case class Count(word: String, count: Int)
-  case class WordsCount(count: Seq[Count])
-  object WordsCount {
-    implicit val monoid: Monoid[WordsCount] = ???
-  }
+      case class WordsCount(count: Seq[Count])
 
-  def countWords(lines: Vector[String]): WordsCount = ???
+      object WordsCount {
+        implicit val monoid: Monoid[WordsCount] = new Monoid[WordsCount] {
+          override def empty: WordsCount = WordsCount(Seq.empty)
+
+          override def combine(x: WordsCount, y: WordsCount): WordsCount = WordsCount(
+            (x.count ++ y.count)
+              .groupBy(z => z.word)
+              .map(word => Count(word._1, word._2.map(item => item.count).sum))
+              .toSeq
+          )
+        }
+      }
+
+
+      def countWords(lines: Vector[String]): WordsCount = {
+        val words = lines.flatMap(_.split(" "))
+        val words_count = mapReduce(words)(word => WordsCount(Seq(Count(word, 1))))
+        Await.result(words_count, 10.second)
+      }
+    }
+  }
 }
