7bcf28e7e2c6849ae6572999ba5f671f530324ac
Kirill Shipulin <shkirkala@gmail.com>
2024-06-12 21:28:30 +0500

    homework_5

--- homeworks/homework_5/src/main/scala/Task4.scala
+++ homeworks/homework_5/src/main/scala/Task4.scala
@@ -1,6 +1,5 @@
 import scala.language.higherKinds
 import scala.util.{Failure, Success, Try}
-
 /*
   Задание №4
   Давайте реализуем свою монаду для обработки ошибок.
@@ -17,11 +16,11 @@ object Task4 extends App {
     def flatMap[A, B](fa: F[E, A])(f: A => F[E, B]): F[E, B]
 
     def map[A, B](fa: F[E, A])(f: A => B): F[E, B] = ???
+    def map[A, B](fa: F[E, A])(f: A => B): F[E, B] = flatMap(fa)(a => pure(f(a)))
 
     def raiseError[A](fa: F[E, A])(error: => E):  F[E, A]
     def handleError[A](fa: F[E, A])(handle: E => A): F[E, A]
   }
-
   case class EIO[+E, +A](value: Either[E, A])
   object EIO {
     def apply[A](value: A): EIO[Nothing, A] = EIO[Nothing, A](Right(value))
@@ -29,17 +28,26 @@ object Task4 extends App {
     def error[E, A](error: E): EIO[E, A] = EIO[E, A](Left(error))
 
     def possibleError[A](f: => A): EIO[Throwable, A] = ???
+    def possibleError[A](f: => A): EIO[Throwable, A] = EIO(Try(f).toEither)
 
     implicit def monad[E]: MonadError[EIO, E] = ???
+    implicit def monad[E]: MonadError[EIO, E] = new MonadError[EIO, E] {
+      override def pure[A](value: A): EIO[E, A] = EIO.apply(value)
+
+      override def flatMap[A, B](fa: EIO[E, A])(f: A => EIO[E, B]): EIO[E, B] =
+        EIO.apply(fa.value.flatMap(a => f(a).value))
+
+      override def raiseError[A](fa: EIO[E, A])(error: => E): EIO[E, A] = EIO.error[E, A](error)
+
+      override def handleError[A](fa: EIO[E, A])(handle: E => A): EIO[E, A] = EIO(fa.value.fold(handle, identity))
+    }
   }
 
   object EIOSyntax {
     implicit class EIOOps[E, A](val eio: EIO[E, A]) {
       def flatMap[B](f: A => EIO[E, B]): EIO[E, B] =
         EIO.monad[E].flatMap(eio)(f)
-
       def map[B](f: A => B): EIO[E, B] = EIO.monad.map(eio)(f)
-
       def handleError(f: E => A): EIO[E, A] =
         EIO.monad.handleError(eio)(f)
     }
