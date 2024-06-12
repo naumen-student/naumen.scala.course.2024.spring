1efc7e1eb62d019641340a4718c16227e372ce54
qreaqtor <bbncvgrgrj@gmail.com>
2024-06-12 23:19:40 +0500

    homework_7

--- homeworks/homework_7/src/main/scala/Breakfast.scala
+++ homeworks/homework_7/src/main/scala/Breakfast.scala
@@ -32,7 +32,28 @@ object Breakfast extends ZIOAppDefault {
   def makeBreakfast(eggsFiringTime: Duration,
                     waterBoilingTime: Duration,
                     saladInfoTime: SaladInfoTime,
-                    teaBrewingTime: Duration): ZIO[Any, Throwable, Map[String, LocalDateTime]] = ???
+                    teaBrewingTime: Duration): ZIO[Any, Throwable, Map[String, LocalDateTime]] =  {
+
+    for {
+      water <- ZIO.sleep(waterBoilingTime).as(LocalDateTime.now()).fork
+      eggs <- ZIO.sleep(eggsFiringTime).as(LocalDateTime.now()).fork
+      salad <- ZIO.sleep(saladInfoTime.tomatoTime.plus(saladInfoTime.cucumberTime)).as(LocalDateTime.now()).fork
+
+      waterTime <- water.join
+
+      tea <- ZIO.sleep(teaBrewingTime).as(LocalDateTime.now()).fork
+
+      eggsTime <- eggs.join
+      saladTime <- salad.join
+      teaTime <- tea.join
+
+    } yield Map(
+      "eggs" -> eggsTime,
+      "water" -> waterTime,
+      "saladWithSourCream" -> saladTime,
+      "tea" -> teaTime
+    )
+  }
 
 
 
