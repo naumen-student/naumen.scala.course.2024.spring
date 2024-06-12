ab3ee74a3a8eddfc4173ac2ba4e71bab7acb1fa1
Ya <toomanycreepers@mail.ru>
2024-06-12 21:08:44 +0500

    homework_5

--- homeworks/homework_5/src/main/scala/Task2.scala
+++ homeworks/homework_5/src/main/scala/Task2.scala
@@ -1,26 +1,30 @@
 import cats._
 import cats.implicits._
 
-/*
-  Задание №2
-  Всё просто, для каждого кейс класса необходимо описать логику его сложения.
-  Радиус-вектор должен складываться, как и любой другой вектор.
-  GradeAngle всегда выражает угол [0, 360).
-  SquareMatrix просто сложение квадратных матриц
- */
 object Task2 extends App {
   case class RadiusVector(x: Int, y: Int)
   object RadiusVector {
-    implicit val monoid: Monoid[RadiusVector] = ???
+    implicit val monoid: Monoid[RadiusVector] = new Monoid[RadiusVector]{
+      override def empty: RadiusVector = RadiusVector(0, 0)
+      override def combine(a: RadiusVector, b: RadiusVector): RadiusVector = RadiusVector(a.x + b.x, a.y + b.y)
+    }
   }
   case class DegreeAngle(angel: Double)
   object DegreeAngle {
-    implicit val monoid: Monoid[DegreeAngle] = ???
+    implicit val monoid: Monoid[DegreeAngle] = new Monoid[DegreeAngle]{
+      override def empty: DegreeAngle = DegreeAngle(0)
+      override def combine(a: DegreeAngle, b: DegreeAngle): DegreeAngle = DegreeAngle((a.angel + b.angel) % 360)
+    }
   }
 
   case class SquareMatrix[A : Monoid](values: ((A, A, A), (A, A, A), (A, A, A)))
   object SquareMatrix {
-    implicit def monoid[A: Monoid]: Monoid[SquareMatrix[A]] = ???
+    implicit def monoid[A: Monoid]: Monoid[SquareMatrix[A]] = new Monoid[SquareMatrix[A]]{
+      override def empty: SquareMatrix[A] = SquareMatrix((Monoid[A].empty, Monoid[A].empty, Monoid[A].empty),
+        (Monoid[A].empty, Monoid[A].empty, Monoid[A].empty),
+        (Monoid[A].empty, Monoid[A].empty, Monoid[A].empty))
+      override def combine(a: SquareMatrix[A], b: SquareMatrix[A]): SquareMatrix[A] = SquareMatrix(a.values.combine(b.values))
+    }
   }
 
   val radiusVectors = Vector(RadiusVector(0, 0), RadiusVector(0, 1), RadiusVector(-1, 1))
--- homeworks/homework_5/src/main/scala/Task3.scala
+++ homeworks/homework_5/src/main/scala/Task3.scala
@@ -5,12 +5,6 @@ import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.DurationInt
 
-/*
-  Задание №3
-  Всё просто, нужно посчитать количество строк.
-  Реализуйте функцию countWords, которая принимает список строк.
-  Обязательно использовать функцию mapReduce.
- */
 object Task3 extends App {
   def mapReduce[A, B: Monoid](values: Vector[A])(func: A => B): Future[B] = {
     val numCores = Runtime.getRuntime.availableProcessors
@@ -25,8 +19,25 @@ object Task3 extends App {
   case class Count(word: String, count: Int)
   case class WordsCount(count: Seq[Count])
   object WordsCount {
-    implicit val monoid: Monoid[WordsCount] = ???
+    implicit val monoid: Monoid[WordsCount] = new Monoid[WordsCount]{
+      override def empty: WordsCount = WordsCount(Seq.empty)
+      override def combine(a: WordsCount, b: WordsCount): WordsCount =
+        WordsCount(
+          (a.count ++ b.count)
+            .groupBy(c => c.word)
+            .map(wordToCount => Count(wordToCount._1, wordToCount._2.map(item => item.count).sum))
+            .toSeq)
+    }
   }
 
-  def countWords(lines: Vector[String]): WordsCount = ???
+  def countWords(lines: Vector[String]): WordsCount = {
+    Await.result(
+      mapReduce(lines)(
+        s => WordsCount(
+          s.split(" ").groupBy(word => word).map(group => Count(group._1, group._2.length)).toSeq
+        )
+      ),
+      10.second
+    )
+  }
 }
