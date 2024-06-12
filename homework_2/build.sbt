04394f9d1085b009ebc677511d992e7e87877006
Grish-Gr <maks_ter23@mail.ru>
2024-06-12 17:07:45 +0300

    homework_7

--- homework_2/build.sbt
+++
@@ -1,8 +0,0 @@
-version := "0.1"
-
-scalaVersion := "2.12.10"
-
-libraryDependencies += "com.lihaoyi" %% "utest" % "0.5.3" % "test"
-libraryDependencies += "org.scala-exercises" %% "definitions" % "0.6.7"
-
-testFrameworks += new TestFramework("utest.runner.Framework")
--- homeworks/homework_7/src/main/scala/Breakfast.scala
+++ homeworks/homework_7/src/main/scala/Breakfast.scala
@@ -1,5 +1,3 @@
-package ru.dru
-
 import zio.CanFail.canFailAmbiguous1
 import zio.{Duration, Exit, Fiber, Scope, ZIO, ZIOApp, ZIOAppArgs, ZIOAppDefault, durationInt}
 
@@ -32,7 +30,26 @@ object Breakfast extends ZIOAppDefault {
   def makeBreakfast(eggsFiringTime: Duration,
                     waterBoilingTime: Duration,
                     saladInfoTime: SaladInfoTime,
-                    teaBrewingTime: Duration): ZIO[Any, Throwable, Map[String, LocalDateTime]] = ???
+                    teaBrewingTime: Duration
+                   ): ZIO[Any, Throwable, Map[String, LocalDateTime]] = {
+    for {
+      water <- ZIO.sleep(waterBoilingTime).as(LocalDateTime.now()).fork
+      eggs <- ZIO.sleep(eggsFiringTime).as(LocalDateTime.now()).fork
+      salad <- ZIO.sleep(saladInfoTime.cucumberTime.plus(saladInfoTime.tomatoTime)).as(LocalDateTime.now()).fork
+
+      waterTime <- water.join
+      tea <- ZIO.sleep(teaBrewingTime).as(LocalDateTime.now()).fork
+
+      eggsTime <- eggs.join
+      saladTime <- salad.join
+      teaTime <- tea.join
+    } yield Map (
+      "eggs" -> eggsTime,
+      "water" -> waterTime,
+      "saladWithSourCream" -> saladTime,
+      "tea" -> teaTime
+    )
+  }
 
 
 
