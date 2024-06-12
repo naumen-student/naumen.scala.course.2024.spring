71274d57bace16952df8a7a2d78b03dfb51dc795
Andrey <100022610+2made1ra@users.noreply.github.com>
2024-06-12 13:49:38 +0500

    Update Breakfast.scala

--- homeworks/homework_7/src/main/scala/Breakfast.scala
+++ homeworks/homework_7/src/main/scala/Breakfast.scala
@@ -32,10 +32,25 @@ object Breakfast extends ZIOAppDefault {
   def makeBreakfast(eggsFiringTime: Duration,
                     waterBoilingTime: Duration,
                     saladInfoTime: SaladInfoTime,
-                    teaBrewingTime: Duration): ZIO[Any, Throwable, Map[String, LocalDateTime]] = ???
+                    teaBrewingTime: Duration): ZIO[Any, Throwable, Map[String, LocalDateTime]] = 
+    for {
+      eggs <- ZIO.sleep(eggsFiringTime).as(LocalDateTime.now()).fork
+      water <- ZIO.sleep(waterBoilingTime).as(LocalDateTime.now()).fork
+      salad <- ZIO.sleep(saladInfoTime.tomatoTime.plus(saladInfoTime.cucumberTime)).as(LocalDateTime.now()).fork
+      tea <- ZIO.sleep(teaBrewingTime).as(LocalDateTime.now()).fork
 
+      eggsTime <- eggs.join
+      waterTime <- water.join
+      saladTime <- salad.join
+      teaTime <- tea.join
+  } yield Map(
+    "eggs" -> eggsTime,
+    "water" -> waterTime,
+    "saladWithSourCream" -> saladTime,
+    "tea" -> teaTime
+  )
 
 
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = ZIO.succeed(println("Done"))
-
 }
+
